c2300801ab7c7afd68807af5d25b33d4c812efd7
shakil8882 <shakilahmmed8882@gmail.com>
2025-02-04 11:15:09 +0600

    feat: add initial JavaScript learning resources & examples

---
+++ Qs/Qs&Ans.js
@@ -0,0 +1,107 @@
+
+/*
+    1.__________Qs: What is javascript?___________
+
+    Ans: JavaScript is varstile programming language, Means we can
+     use it in backend(nodejs) and frontend(browser), Mobile(react 
+     native), Desktop(electronJS).
+    
+     It is multi-paradigm programming language, Means we can use 
+     it in different paradigms like functional programming (declarative, HOF, Pure function), 
+     object-oriented programming (class, object, inheritance, polymorphism, abstraction, encapsulation), 
+     procedural programming (function, loop, conditional statement).
+
+
+
+
+     2. __________Qs: What file extension is
+      used for javascript?___________
+
+     Ans: .js (javascript standard file extension)
+     .jsx (javascript extension for react)
+     .ts (typescript file extension)
+     .tsx (typescript extension for react)
+
+
+     3. __________Qs: What is the variable___________
+        Ans: Variable is a container that holds a value.
+
+
+     4. __________Qs: What is the difference 
+     between var, let and const?___________
+
+     Ans: var is function scoped, let and const are block scoped. 
+     var is hoisted, let and const are not hoisted. var can be 
+     redeclared, let and const cannot be redeclared. var & let can be reassigned but const cannot be reassigned.
+
+
+     5. __________Qs: What is literal?___________
+
+     Ans: Literal is a fixed value in code.
+     Example:
+      1, "hello"__ string literal, true, false__ boolean literal, null, undefined, [1,2,3]__ array literal, {name: "John", age: 20}__ object literal, () => {}, etc.
+
+
+
+     6. __________Qs: What is the difference 
+     between var, let and const?___________
+
+     Ans: var is function scoped, let and const are block scoped. 
+     var is hoisted, let and const are not hoisted. var can be 
+     redeclared, let and const cannot be redeclared. var & let can be reassigned but const cannot be reassigned.
+
+    
+     7. __________Qs: What is camel case?___________
+
+     Ans: Camel case is a naming convention for variables. 
+     word is capitalized except the first word.
+
+
+     
+     8. __________Qs: What is Expression?___________
+
+     Ans: Expression is a piece of code that evaluates to a value.
+     "It gives me something" is an expression.
+        Whenever you see an expression, ask yourself: Does this give me something (a value)?
+        If it does, it’s an expression.
+
+        Example: 2 + 3 → gives you 5 → Expression
+        Example: let x = 10 → you assign a value, but it doesn't give you a value directly → Not an expression (it's a statement).
+
+        here let x = 10 is a statement, it doesn't give you a value directly.
+        but x is an expression, it gives you a value 10.
+        ____console.log(x) ___ 
+        this whole above line is a statement but individually x is an expression as it evaluates to a value which is 10.
+
+        Whenever you write a line of code, ask:
+        "Is this line producing a value?"
+        If it is (like a variable or operation), it's an expression.
+        If it's doing something (like assigning or printing), it's a statement.
+
+    9. __________Qs: 
+        Ans: 
+    10. __________Qs: 
+        Ans: 
+
+
+___________________________________________________________________
+___________________________________________________________________
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+*/
