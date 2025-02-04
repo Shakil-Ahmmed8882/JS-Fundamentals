2e4997844651d754d5f388312599398887796035
shakil8882 <shakilahmmed8882@gmail.com>
2025-02-04 10:27:16 +0600

    first commit

---
+++ chapter-one/0.1.js
@@ -0,0 +1,29 @@
+
+
+/*
+what is data type?
+-> data type is the type of data that a variable can hold.
+There are two types of data type:
+1. primitive data type
+2. non-primitive data type
+*/
+//
+
+
+// __________primitive__________
+// number
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+
+// __________non-primitive__________
+// Object
+// Array
+// Function
+
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
